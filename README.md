cf4c735704a3e6ac8cbd49ee3d197adbbcf36341
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-04-27 17:06:24 -0700

    testing gpg

---
+++ README.md
@@ -0,0 +1,1 @@
+A repo containing all the grinding I had to do in order to pass code tests.
--- src/leetCode/algosQ/1-twoSums.py
+++ src/leetCode/algosQ/1-twoSums.py
@@ -4,7 +4,8 @@ from copy import deepcopy
 class Solution:
 
 
-    def twoSum(self, nums: list, target: int) -> list:
+    # Brute Force solution
+    def twoSumBrute(self, nums: list, target: int) -> list[int]:
         """
         Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
         You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -16,7 +17,13 @@ class Solution:
             for idx2, subNumber in enumerate(subNums):
                 if (target - number) == subNumber:
                     return [idx,idx2]
+    
+    #hash table
+    def twoSumHash(self,nums: list, target: int) -> list[int]:
+        """
 
+        """
+    
 solution = Solution()
-print(solution.twoSum([3,2,4],6))
+print(solution.twoSumBrute([3,2,4],6))
 # print(2+2)
