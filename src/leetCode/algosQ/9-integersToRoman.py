a5ea2e44595599e50d18f2563273e39ebce1cf3e
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-06-01 14:47:27 -0700

    created pattern

--- src/leetCode/algosQ/9-integersToRoman.py
+++ src/leetCode/algosQ/9-integersToRoman.py
@@ -11,7 +11,20 @@ class Solution:
         M             1000
         https://www.rapidtables.com/math/symbols/roman_numerals.html
         """
+        a,b,c = ["","",""] # e.g. 0-10 -> a="I",b="V"c="X"
         importantNumbs = {
+                1:a,
+                2:2*a,
+                3:3*a,
+                4:a+b,
+                5:b,
+                6:b+a,
+                7:b+2*a,
+                8:b+3*a,
+                9:a+c
+                }
+        importantNumbs2 = ["I","V","X","L","C","D","M"] #1,5,10,50,100,500,1000
+        pattern1 = {
                 1:"I",
                 2:"II",
                 3:"III",
@@ -20,24 +33,27 @@ class Solution:
                 6:"VI",
                 7:"VII",
                 8:"VIII",
-                9:"IX",
+                9:"IX"
+                }
+        pattern2 = {
                 10:"X",
-                40:"XL",
+                20:"XX",
+                30:"XXX",
+                40:"XV",
                 50:"V",
-                90:"XC",
-                10:"C",
-                400:"CD",
-                500:"D",
-                900:"CM",
-                1000:"M"
+                60:"VX",
+                70:"VXX",
+                80:"VXXX",
+                90:"XC"
                 }
         # seesm that solution could be using 1-9 but being wary of which order (10, 100, 1000) they are placed.
+
         strValue = str(num)
         sizeNum = len(strValue)
         romans = []
         for i in strValue:
             if int(i)*10**sizeNum in importantNumbs:
-                romans.append(importantNumbs[i])
+                romans.append(importantNumbs[int(i)])
 
 
 
